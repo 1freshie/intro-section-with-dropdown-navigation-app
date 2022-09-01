4f26d18973a0e93b17b7a327103fa500e4c8666b
1freshie <hristian.todorov14@gmail.com>
2022-09-01 14:49:04 +0300

    first react-typescript project init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+  // Features
+  // Company
+  // Careers
+  // About
+
+  // Login
+  // Register
+
+  // Make remote work
+
+  // Get your team in sync, no matter your location. Streamline processes, 
+  // create team rituals, and watch productivity soar.
+
+  // Learn more
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
