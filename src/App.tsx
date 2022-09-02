b3e13bf9f7e501e514f39089120b5c0c48c69283
1freshie <hristian.todorov14@gmail.com>
2022-09-02 10:33:37 +0300

    added dropdown menu, needs fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import audiophile from './images/client-audiophile.svg';
 import databiz from './images/client-databiz.svg';
 import maker from './images/client-maker.svg';
 import meet from './images/client-meet.svg';
+import arrow_up from './images/icon-arrow-up.svg';
+import arrow_down from './images/icon-arrow-down.svg';
+import calendar from './images/icon-calendar.svg';
+import reminders from './images/icon-reminders.svg';
+import planning from './images/icon-planning.svg';
+import todo from './images/icon-todo.svg';
+import { useState } from 'react';
 
 // Features
 // Company
@@ -21,17 +28,57 @@ import meet from './images/client-meet.svg';
 // Learn more
 
 const App: React.FunctionComponent = () => {
+  const [isFeaturesShown, setIsFeaturesShown] = useState(false);
+  const [isCompanyShown, setIsCompanyShown] = useState(false);
+
+  const showDropdownHandler = (id: string) => {
+    id === 'features'
+      ? setIsFeaturesShown((prevShown) => !prevShown)
+      : setIsCompanyShown((prevShown) => !prevShown);
+  };
+
   return (
     <>
       <div className="navbar">
         <div className="container">
           <img src={logo} alt="logo" className="logo" />
           <ul>
-            <li>
-              <a href="#">Features</a>
+            <li className="dropdown">
+              <button
+                className="dropbtn"
+                onClick={() => showDropdownHandler('features')}
+              >
+                Features
+              </button>
+              <div
+                className={
+                  !isFeaturesShown
+                    ? 'dropdown-content'
+                    : 'dropdown-content show'
+                }
+              >
+                <a href="#">Todo List</a>
+                <a href="#">Calendar</a>
+                <a href="#">Reminders</a>
+                <a href="#">Planning</a>
+              </div>
             </li>
-            <li>
-              <a href="#">Company</a>
+            <li className="dropdown">
+              <button
+                className="dropbtn"
+                onClick={() => showDropdownHandler('company')}
+              >
+                Company
+              </button>
+              <div
+                className={
+                  !isCompanyShown ? 'dropdown-content' : 'dropdown-content show'
+                }
+              >
+                <a href="#">History</a>
+                <a href="#">Our Team</a>
+                <a href="#">Blog</a>
+              </div>
             </li>
             <li>
               <a href="#">Careers</a>
